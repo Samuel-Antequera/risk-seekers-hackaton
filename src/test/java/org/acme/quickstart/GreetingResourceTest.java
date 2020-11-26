ea00a643e4c907ccaf98a5342ba509e59c89ced5
Samuel <samuelworker@protonmail.com>
2020-11-26 08:57:16 +0100

    Se modifican los tests

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -11,7 +11,7 @@ public class GreetingResourceTest {
 
 	@Test
 	public void testHelloEndpoint() {
-		given().when().get("/hello").then().statusCode(200).body(is("hello"));
+		given().when().get("/wise-wallet/users").then().statusCode(200).body(is("listado de usuarios"));
 	}
 
 }
